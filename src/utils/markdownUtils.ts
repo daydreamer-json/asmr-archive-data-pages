03b2499da483559464eb5471ce4bc8c4e7062b10
daydreamer-json <atarakima1sudo@gmail.com>
2024-11-12 02:03:25 +0900

    Fix css/js load issue

--- src/utils/markdownUtils.ts
+++ src/utils/markdownUtils.ts
@@ -36,11 +36,16 @@ function genHtmlTextSingleWork(
     <style>
       /* @import url('https://cdn.jsdelivr.net/gh/daydreamer-json/SanFranciscoFontCDN@main/sanfrancisco.min.css'); */
       @import url('https://cdn.jsdelivr.net/npm/bootstrap@5.3/dist/css/bootstrap.min.css');
+      @import url('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css');
+      @import url('https://cdn.vidstack.io/player/theme.css');
+      @import url('https://cdn.vidstack.io/player/video.css');
+      @import url('https://cdn.vidstack.io/player/audio.css');
       @import url('https://rsms.me/inter/inter.css');
       @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Noto+Sans+JP:wght@100..900&family=Noto+Sans+SC:wght@100..900&display=swap');
       @import url('../../assets/css/work.css');
     </style>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.vidstack.io/player" type="module"></script>
   </head>
   <body>
     <div class="my-4 container px-4" id="mainContainer"><h1 class="text-break" id="work-page-title">---</h1><hr class="my-3"><a id="work-coverImage-link" rel="noopener noreferrer" target="_blank"><img class="rounded" class="my-4" alt="Cover Image" id="work-coverImage-img" src="../../assets/img/cover_main_dummy/small/purple.webp" style="max-width:100%"></a><hr class="my-3"><h2>Work Info</h2><table class="align-middle table table-hover table-sm"><tr><th>ID<td class="font-monospace"><a id="work-data-info-id" rel="nofollow">---</a><tr><th>Title<td class="text-break" id="work-data-info-workTitle">---<tr><td>Circle<td class="text-break"><span id="work-data-info-circleName">---</span> (<a id="work-data-info-circleLink" rel="nofollow" class="font-monospace">---</a>)<tr><td>VAs<td class="text-break" id="work-data-info-vas">---<tr><td>Tags<td class="text-break" id="work-data-info-tags">---<tr><td>Age restrict<td id="work-data-info-ageCategoryString">---<tr><td>Price<td class="font-monospace" id="work-data-info-price">---<tr><td>Released<td class="font-monospace" id="work-data-info-releasedAt">---<tr><td>Created<td class="font-monospace" id="work-data-info-createdAt">---<tr><td>Added<td class="font-monospace" id="work-data-info-date">---</table><hr class="my-3"><h2>File List</h2><ul class="ps-4" id="work-fileList-parent"></ul><div class="fade modal" id="player-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"><div class="modal-dialog modal-dialog-centered modal-dialog-scrollable"><div class="modal-content"><div class="modal-header"><p class="text-break modal-title" id="player-modal-title"></p><button class="btn-close" id="player-modal-close" type="button"></button></div><div class="modal-body" id="player-modal-body"><div class="d-flex flex-column justify-content-center w-100" id="player-modal-body-flex"><div id="vidstack-target"></div></div></div><div class="modal-footer"><a id="player-modal-download-btn" rel="noopener noreferrer" class="btn btn-primary" target="_blank"><i class="bi bi-box-arrow-up-right me-2"></i>Download</a></div></div></div></div></div>
